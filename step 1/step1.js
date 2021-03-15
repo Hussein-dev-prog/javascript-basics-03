6219d3f5d99299460c207879976ee474978658c4
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-03-15 13:22:30 +0200

    completed step-1

---
+++ step 1/step1.js
@@ -0,0 +1,7 @@
+var st = document.querySelector("#image1");
+st.addEventListener("mouseover",()=>{
+    st.style.border = "3px solid red";
+})
+st.addEventListener("mouseout",()=>{
+    st.style.border = "none";
+})
